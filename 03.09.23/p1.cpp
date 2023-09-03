3f58b046630ddd3bf587f663584b5660ee45786f
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-09-03 17:58:22 +0200

    first commit

---
+++ 03.09.23/p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+    double pi=3.14159;
+    double radius=10;
+    double result=pi+radius;
+    int number=2;
+    int result_number=number;
+
+
+
+    std::cout<<result;
+    std::cout<<result_number;
+    std::cout<<"wynik"<<std::endl;
+    std::cout<<"13";
+    std::cout<<result_number;
+
+
+}
