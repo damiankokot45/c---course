60dd8d095a3e66c2b51ad5e9334fcf2397cdb309
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-10-04 13:47:12 +0200

    Arithmetic operators

--- 03.09.23/p2.cpp
+++ 03.09.23/p2.cpp
@@ -4,7 +4,7 @@ int main(){
 
 int liczba=2;
 int wynik=0;
- for (int i = 0; i < 10; i++)
+ for (int i = 0; i < 2; i++)
  {
     wynik=liczba*i;
     std::cout<<wynik<<std::endl;
---
+++ 6h course/dzialania.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    int student=20;
+    //student+=2:
+    //student++;
+    //student-=2;
+    //student--;
+    //student*=2;
+    //student/=2;
+
+    //40 minutes
+
+    //for (int i = 0; i < 5; i++)
+    //{
+        //student--;
+    //}
+    
+    std::cout<<student<<std::endl;
+}
