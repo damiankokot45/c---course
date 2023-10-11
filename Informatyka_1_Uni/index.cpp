387ebb780a4ba14e2f75594087ff9ddaa56a6b85
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-10-11 20:20:36 +0200

    commit

---
+++ Informatyka_1_Uni/index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hello World!"<<std::endl;
+    //typy zmiennych
+    int a=10; //typ całkowity
+    float b=10.10; //typ przecinkowy
+    bool c=true; //true/ false
+    char ocena='A'; //wyrazy/znaki pojedynczy
+    double d=32.32; //zmienno przecinkowy
+    std::string imie="Damian";
+}
