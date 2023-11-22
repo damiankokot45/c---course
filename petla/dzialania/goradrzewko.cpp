1d1dca8774e9535f9648e410cf2d71d448bb6367
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-11-22 14:32:10 +0100

    update

--- petla/dzialania/goradrzewko.cpp
+++ petla/dzialania/goradrzewko.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main() {
-    int n = 5; // liczba wierszy (zmień według potrzeb)
+    int n = 10; 
 
     for (int i = 0; i < n; ++i) {
         for (int j = n; j > i + 1; --j) {
             std::cout << " "; // dodaj spacje przed trójkątem
         }
-        for (int k = 0; k < 2 * (i + 1) - 1; ++k) {
+        for (int m = 0; m < 2 * (i + 1) - 1; ++m) {
             std::cout << "*"; // rysuj trójkąt
         }
-        std::cout << std::endl;
+        std::cout<<std::endl;
     }
 
     return 0;
--- petla/liczbyparzyste.cpp
+++ petla/liczbyparzyste.cpp
@@ -1,18 +1,17 @@
 //Napisz program wypisujący liczby całkowite parzyste od liczby podanej przez użytkownika aż do 0.
 
 #include <iostream>
-using namespace std;
  
 int main()
 {
     int liczba;
-    cout << "Podaj liczba: ";
-    cin >> liczba;
+    std::cout << "Podaj liczba: ";
+    std::cin >> liczba;
  
     do
     {
         if (liczba%2==0)
-            cout << liczba << " ";
+            std::cout << liczba << " ";
         liczba--;
     }
     while(liczba>=0);
