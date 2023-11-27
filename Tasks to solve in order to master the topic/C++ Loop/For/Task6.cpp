9c09eae129f5f0deea7f486c873c0bbc33cce004
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-11-27 22:09:15 +0100

    update

---
+++ Tasks to solve in order to master the topic/C++ Loop/For/Task6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    int y=0;
+    int wynik=0;
+    std::cin>>y;
+
+    for (int i = 0; i < 10; i++)
+    {
+        wynik=y*y;
+    }
+
+    std::cout<<wynik;
+    
+}
